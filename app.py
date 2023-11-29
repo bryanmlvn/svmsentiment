c18f71ec351a47ff41c01d6e14ca2a32e8dc4e41
bryanmlvn <122680267+bryanmlvn@users.noreply.github.com>
2023-11-29 18:32:46 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,10 +7,9 @@ from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.linear_model import SGDClassifier
 
- # Page Title and Sidebar Logo
- st.set_page_config(
-     page_title="Sentiment Analysis", page_icon="🔥", layout="centered"
- )
+st.set_page_config(
+ page_title="Sentiment Analysis", page_icon="🔥", layout="centered"
+)
 
 # # Sidebar
 
